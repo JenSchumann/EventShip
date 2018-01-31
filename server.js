6f2071ad98b7a9fdb0ab8f8c6bb25a416122b863
Jen Schumann <jenniferschumann1@gmail.com>
2018-01-31 12:31:20 -0700

    installed dotenv package

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
+const env = require('dotenv').config();
 
 app.use(express.static('public'));
+
+app.use(session({
+	secret: process.env.SECRET,
+	resave: false,
+	saveUninitialized: false
+}))
 
 
 app.listen(3000, ()=>{
